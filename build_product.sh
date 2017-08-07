1ad5e5621069a7444d058da70d4ea9cd81d7b290
sulirong <sulirong@baidu.com>
2017-08-07 10:26:45 +0800

    Modify build script

--- build_product.sh
+++ build_product.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 clear
 shellpath=$(cd `dirname $0`; pwd)
+groupOut=$shellpath'/groupOut'
+cd $shellpath
+
+rm -r $groupOut
+mkdir $groupOut
+mkdir $groupOut'/aar'
+mkdir $groupOut'/apk'
+
 ./gradlew clean --info --stacktrace
 ./gradlew assembleRelease --info --stacktrace
+cp -f ./app/build/outputs/apk/release/app-release.apk  $groupOut'/apk/app-release.apk'
